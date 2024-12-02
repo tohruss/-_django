7ee7c12f455f0ebc0bce565226b3dfdbeca7aad3
tohruss <taraskind6@gmail.com>
2024-12-02 19:43:55 +0700

    создание домашней страницы

--- mytestsite/catalog/views.py
+++ mytestsite/catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Book, BookInstance, Author, Genre
 
-# Create your views here.
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.count()  # Общее количество книг
+    num_instances = BookInstance.objects.count()  # Общее количество экземпляров книг
+    num_instances_available = BookInstance.objects.filter(status='a').count()  # Доступные книги (статус = 'a')
+    num_authors = Author.objects.count()  # Общее количество авторов
+    num_genres = Genre.objects.count()  # Общее количество жанров
+
+
+    # Отрисовка HTML-шаблона index.html с данными внутри переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors,
+            'num_genres': num_genres,
+        },
+    )
+
